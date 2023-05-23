da0292de7055788172fc8354e329ca42e7c4cef7
Anshul Agrawal <anagrawal@bestpeers.com>
2023-05-23 19:11:24 +0530

    blog modification

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,14 @@
 class Article < ApplicationRecord
     include Visible
-  
+    before_validation :capitalize
     has_many :comments, dependent: :destroy
   
     validates :title, presence: true
+    
     validates :body, presence: true, length: { minimum: 10 }
+    private
+    def capitalize 
+      self.title=self.title.downcase.titleize
+    end
   end
   
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,10 @@
 class Comment < ApplicationRecord
   include Visible
-
+  before_validation :camel_case
   belongs_to :article
+  private
+
+  def camel_case
+   self.body[0]=self.body[0].titleize
+  end 
 end
