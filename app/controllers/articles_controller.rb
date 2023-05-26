ffbc77e38164192ff3b951979cc1f03acab850f2
Anshul Agrawal <anagrawal@bestpeers.com>
2023-05-26 16:26:39 +0530

    association

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,7 @@ class ArticlesController < ApplicationController
   
   def index
     @articles = Article.all
+    @masters = Master.all
   end
 
   def show
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_25_113037) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_26_094748) do
+  create_table "account_histories", force: :cascade do |t|
+    t.integer "credit_rating"
+    t.integer "account_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["account_id"], name: "index_account_histories_on_account_id"
+  end
+
   create_table "accounts", force: :cascade do |t|
     t.string "account_no"
     t.integer "supplier_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["supplier_id"], name: "index_accounts_on_supplier_id"
+  end
+
+  create_table "appointments", force: :cascade do |t|
+    t.date "date"
+    t.integer "physician_id", null: false
+    t.integer "patient_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["patient_id"], name: "index_appointments_on_patient_id"
+    t.index ["physician_id"], name: "index_appointments_on_physician_id"
   end
 
   create_table "articles", force: :cascade do |t|
@@ -39,12 +57,27 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_25_113037) do
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
+  create_table "patients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "physicians", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "suppliers", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "account_histories", "accounts"
   add_foreign_key "accounts", "suppliers"
+  add_foreign_key "appointments", "patients"
+  add_foreign_key "appointments", "physicians"
   add_foreign_key "comments", "articles"
 end
